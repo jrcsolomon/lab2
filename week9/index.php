4ae4a5ed5bb664931c9fcf0d352d8b424981a747
John Rafael Solomon <jcsolomon@student.apc.edu.ph>
2023-02-26 22:02:17 +0800

    New Website + PHP + mySQL

--- week9/index.php
+++ week9/index.php
@@ -1,133 +1,53 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-<title>My Profile</title>
-<link rel="icon" type="image/x-icon" href="https://kanji.jitenon.jp/shotai3/019.gif">
-<!-- CSS CONTAINER -->
-<style>
-body{
-margin-left: 20px;
-margin-right: 20px;
-font-family: Calibri, Verdana;
-border: 2px solid gray;
-background-color: 	#b3cde0;
-}
-
-.profile{
-text-align:left;
-vertical-align: top;
-font-size: 17px; 
-display:inline-block;
-width:300px;
-float:left;
-text-indent:20px;
-}
-
-table, th{
-  border: 1px solid;
-}
+	<title>My Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" re="stylesheet">
 	
-#maincontent{
-text-align:center;
-background-color: #fff;;
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" 
+	integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-}
-
-.content{	
-vertical-align: middle;
-display:block;
-float:inherit;
-}
-
-.contentinfo {
-display: inline-block;
-margin: 10px;
-}
-
-a{
-text-decoration: none;
-font-size: 20px;
-}
-
-a:link{
-color: blue;
-}
-a:hover{
-color: green;
-}
-a:active{
-color: darkblue;
-}
-
-h2 {
-text-indent: 30px;
-}
-
-
-</style>
+	
 </head>
-<!-- MAIN BODY -->
+
 <body>
-
-
-<div class="profile">
-<h1 style="background-color: #2e51a2; color: white; font-family:Verdana; font-size: 25px">My Profile</h1>
-<img src="https://user-content.flat-cdn.com/6207e2f033f823001243e379/9a25cb3d-322c-4fda-ab93-d66317496289.jpeg" width="150px" style=" display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 60%;">
-  <br>
- 
-  
-<table style="background-color: rgb(245,245,245);margin:auto; ">
-<tr> <th>John Rafael C. Solomon</th></tr>
-<tr><th>EMC191</th></tr>
-<tr><th>WEBPROG</th></tr>
-</table>
-
- <a href="account.php" style="display: block;text-align: center;">Account Form</a>
-</div>
-<div class="content">
-<h1 style="background-color:#2e51a2;font-family:Verdana; color: white;font-size: 25px;text-indent:20px;">My Games</h1>
-<div id="maincontent">
-
-
-
-<a href="https://bluearchive.nexon.com/home">
-<div class="contentinfo">
-<img src="https://i.imgur.com/5Ov7trk.jpeg" alt="Shiroko" width="300"><br>
-Blue Archive
-</div>
-</a>
-
-<a href="https://umamusume.jp/">
-<div class="contentinfo">
-<img src="https://cdn.animenewsnetwork.com/thumbnails/max500x600/encyc/A20598-2494179646.1520139828.jpg" alt="Uma" width="300">
-<br>Uma Musume: Pretty Derby
-</div>
-</a>
-
-<a href="https://shinycolors.enza.fun/">
-<div class="contentinfo">
-<img src="https://news-pctr.c.yimg.jp/uUzvQ3lML_bkIqyakc1vFhNrRI0RUQxg5aFkrX0xDg1_T0wXrbEJjtNGtrf1o9y3CflXDDJ_ISqZnnmAYTi4dzCRNORshzBUxCAK0FPKpX_PMZ22Y-iVfbXU25Divmql0W0pQocPRS93kbIWN50k7YGXgKzzDluUJOzVBBizLK33D0ZqAonAUT8jH69RKMxG_9cFJr6tYoB-YdIMtialYw==" alt="Shinymas" width="500">
-<br>Idolm@ster: Shiny Colors
-</div>
-</a>
-<br>
-</div>
-</div>
+	
+	<div class="hero">
+		<nav>
+			<h2 class="logo">My<span>Profile</span></h2>
+			<ul>
+			<li><a href="index.php">About Me</a></li>
+			<li><a href="favorites.php">My Favorites</a></li>
+			<li><a href="#">Art and Photography</a></li>
+			<li><a href="guests.php">Guests</a></li>
+			<li><a href="contact.php">Contact</a></li>
+			</ul>
+			
+			<a href="#" class="btn">Subscribe</a>
+			
+		</nav>
+		
+		<div class="content">
+			<h4>Hello, I am</h4>
+			<h1><span>John Rafael</span> Solomon</h1>
+			<h3>EMC191</h3>
+		
+		
+		</div>
+		<div class="share">
+			<p>Lets Connect</p>
+		
+			<div class="social">
+			<a href="#"><i class="fa-brands fa-facebook"></i></a>
+			<a href="#"><i class="fa-brands fa-twitter"></i></a>
+			<a href="#"><i class="fa-brands fa-discord"></i></a>
+			<a href="#"><i class="fa-brands fa-youtube"></i></a>
+			</div>	
+		</div>
+	</div>
+	
 
 </body>
-
-<!-- git update command cycle:
-git status
-git add *
-git commit -m "x"
-git push 
-
-then git pull on server bash on public_html\lab2\week3
--->
-
-
 </html>
---
+++ week9/insertdata.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "testdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO MyGuests (name, email, website)
+VALUES ('John Rafael Solomon', 'johnrafaelsolomon@gmail.com', 'localhost')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+</body>
+
+
+
+
+
+</html>
+
